20077b6fd130bf41af8eed91f8d02e00e50a95fc
Jakub Koza <jakub.koza13@gmail.com>
2022-06-28 23:38:36 +0200

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginViewComponent } from './components/login-view/login-view.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginViewComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
